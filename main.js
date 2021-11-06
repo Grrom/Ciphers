edd3b61e1a528c1b3a725900255a4217c90cdb1e
Jerome <josejeromelalunio@gmail.com>
2021-11-06 11:31:55 +0800

    add keyword encrypt

--- main.js
+++ main.js
@@ -2,11 +2,32 @@ let strings = require("./strings");
 let alphabet = require("./alphabet");
 
 function caesarEncrypt(word, shift) {
-  let newWord = "";
   let shiftedAlphabet = alphabet.slice();
   for (let i = 1; i <= shift; i++) {
     shiftedAlphabet.push(shiftedAlphabet.shift());
   }
+
+  return encrypt(shiftedAlphabet, word);
+}
+
+function keywordEncrypt(word, keyword) {
+  let newWord = "";
+  let shiftedAlphabet = alphabet.slice();
+  for (let i = 0; i < keyword.length; i++) {
+    shiftedAlphabet.splice(i, 0, keyword[i]);
+  }
+  for (let i = keyword.length; i < shiftedAlphabet.length; i++) {
+    if (keyword.includes(shiftedAlphabet[i])) {
+      shiftedAlphabet[i] = null;
+    }
+  }
+  shiftedAlphabet = shiftedAlphabet.filter((value) => value !== null);
+
+  return encrypt(shiftedAlphabet, word);
+}
+
+function encrypt(shiftedAlphabet, word) {
+  let newWord = "";
   for (let i = 0; i < word.length; i++) {
     if (word[i] == " ") {
       newWord += " ";
@@ -19,3 +40,7 @@ function caesarEncrypt(word, shift) {
 }
 
 strings.forEach((string) => console.log(caesarEncrypt(string, 3)));
+strings.forEach((string) => console.log(keywordEncrypt(string, "METHODS")));
+
+// console.log(caesarEncrypt("DEFEND THE EAST WALL OF THE CASTLE", 1));
+// console.log(keywordEncrypt("UNIVERSITY", "COLEG"));
